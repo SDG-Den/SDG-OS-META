95a41af23f0d923a471d4922ea0ead8cd1ac853d
SDGDen <sdgden@sdgcloud.nl>
2026-07-10 12:55:24 +0200

    auto-update

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# requires sdgpkg to already be bootstrapped
+sdgpkg uninstall unipkg
+sdgpkg uninstall sdg-fetch
+sdgpkg uninstall sdg-docs
+sdgpkg uninstall sdg-mango
+sdgpkg uninstall sdg-tips
+sdgpkg uninstall sdg-util
+sdgpkg uninstall sdg-themes
+sdgpkg uninstall sdg-wayshell
+sdgpkg uninstall sdg-wayshell-conf
+sdgpkg uninstall sdg-vox
+sdgpkg uninstall sdg-glyphs
+sdgpkg uninstall sdg-mango-helpers
---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# requires sdgpkg to already be bootstrapped
+sdgpkg install unipkg
+sdgpkg install sdg-fetch
+sdgpkg install sdg-docs
+sdgpkg install sdg-mango
+sdgpkg install sdg-tips
+sdgpkg install sdg-util
+sdgpkg install sdg-themes
+sdgpkg install sdg-wayshell
+sdgpkg install sdg-wayshell-conf
+sdgpkg install sdg-vox
+sdgpkg install sdg-glyphs
+sdgpkg install sdg-mango-helpers
